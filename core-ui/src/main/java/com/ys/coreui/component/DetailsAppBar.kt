33c7b05523c30d5229a591df49eadf5c78dc1cc8
YS <yoonsam86@gmail.com>
2024-11-06 15:04:39 +0900

    core-ui module 추가 및 원형 프로필, 검색홈바 컴포넌트 추가

---
+++ core-ui/src/main/java/com/ys/coreui/component/DetailsAppBar.kt
@@ -0,0 +1,20 @@
+package com.ys.coreui.component
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+//@Composable
+//fun DetailsAppBar(
+//    modifier: Modifier,
+//    navController: NavController
+//) {
+//    Row(
+//        modifier = modifier.fillMaxWidth(),
+//        verticalAlignment = Alignment.CenterVertically
+//    ) {
+//
+//    }
+//}
